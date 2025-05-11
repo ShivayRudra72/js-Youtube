52657de8b93a60170550a653ec43af47023d768a
ShivayRudra72 <sainisecret685@gmail.com>
2025-05-11 11:16:10 +0000

    datatype

---
+++ Basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+//NULL COMPARISON
+console.log(null > 0); //f
+console.log(null < 0); //f
+console.log(null == 0); //f
+console.log(null >= 0); //comparison null to number it treated as-->0 //T
+
+//UNDEFINED COMPARISON
+console.log(undefined == 0); //f
+console.log(undefined > 0);  //f
+console.log(undefined < 0);  //f
+
+
+
+
+
+
+
+
