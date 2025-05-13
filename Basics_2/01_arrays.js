52b6825ce5b496122db1e3521c6b340deacb9d04
ShivayRudra72 <sainisecret685@gmail.com>
2025-05-13 12:44:26 +0000

    array

---
+++ Basics_2/01_arrays.js
@@ -0,0 +1,43 @@
+// ++++++++++++++++++++++ ARRAYS ++++++++++++++++++++++++ //
+//array declarations
+const myArr = [0 ,1, 2, 3 ,5, 50]
+console.log(myArr); // [ 0, 1, 2, 3, 5, 50 ]
+
+const myArr2 = (1,2,3,4,5,6,7,8)
+console.log(myArr2);
+
+// creation of object : and then print
+const arr = new Array(1,0,98,78)
+console.log(arr);
+
+
+
+// ++++++ ARRAYS METHODS ++++++++++ //
+const dev = ["spiderman" , "captain" , "furious" , "thor"];
+
+//operations
+dev.push("ro") // add element
+console.log(dev); // [ 'spiderman', 'captain', 'furious', 'thor', 'ro' ]
+
+
+dev.pop() // in pop : last element will be deleted
+console.log(dev); // [ 'spiderman', 'captain', 'furious', 'thor' ]
+
+
+dev.unshift(900) // it can add in 900 on the position of : "900"
+console.log(dev); // [ 900, 'spiderman', 'captain', 'furious', 'thor' ]
+
+dev.shift()
+console.log(dev); // [ 'spiderman', 'captain', 'furious', 'thor' ] : 900 is deleted
+
+
+
+// IN JS HOW YOU KNOW IN ARRAY THE ELEMENT IS : PRESENT OR NOT
+console.log(dev.includes("thor")); // true
+console.log(dev.indexOf("thor")); // 3
+
+console.log(dev); 
+
+
+
+
