b0c1d7bdfd330650ee3893742a9656a71674bd57
RUDRA PRAKASH SHRIMALI <sainisecret685@gmail.com>
2025-07-27 16:01:22 +0000

    three

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -5,6 +5,10 @@
 // [{ }, { }, { }, { }, { }, { }]
 
 // FOR OF 
+//syntax of for of loop
+// for (const element of object) {
+    
+// }
 const arr = [0,1,2,3,4,5]
 for (const mahadev of arr) {
     console.log(mahadev);
@@ -19,3 +23,16 @@ for (const greet of greetings) {
 //USES OF MAP
 // Map is a key value pair , no duplicate value , all unique values
 // remembering the original insertion order of the keys
+const map = new Map()
+map.set('IN',"India");
+map.set('US',"United State Of America");
+map.set('FR',"France");
+
+//console.log(map);
+
+// agr mere ko map print krana hai array sai to use this
+for (const [key, value] of map) {
+    console.log(key, ':-' , value);
+}
+
+
